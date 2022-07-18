f56036e71c24f785e268b9c94c2355132816e697
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-07-19 01:34:46 +0800

    Added /vk: settings for the plugin

---
+++ src/main/kotlin/com/vinkami/vinkamilang/command/Commands.kt
@@ -0,0 +1,44 @@
+package com.vinkami.vinkamilang.command
+
+import com.vinkami.vinkamilang.PathFinder
+import org.bukkit.entity.Player
+
+class Commands(val pf: PathFinder) {
+    fun execute(sender: Player, cmd: String): Pair<Boolean, String> {
+        val things: List<String> = cmd.split(" ")
+
+        return when (things[0]) {
+            "/fly" -> this.fly(sender, things.drop(1))
+            "/vk" -> this.vk(sender, things.drop(1))
+
+            else -> false to ""  // Not a command from here
+        }
+    }
+
+    private fun fly(sender: Player, args: List<String>): Pair<Boolean, String> {
+        if (args.isNotEmpty()) {return true to "No arguments for /fly"}
+
+        return if (sender.allowFlight) {
+            sender.allowFlight = false
+            true to "Flying is disabled"
+        } else {
+            sender.allowFlight = true
+            true to "Flying is enabled"
+        }
+    }
+
+    private fun vk(sender: Player, args: List<String>): Pair<Boolean, String> {
+        if (args.isEmpty()) {return true to "Usage: /vk [reload|scripts]"}
+
+        return when (args[0]) {
+            "reload" -> {
+                this.pf.reloadScripts()
+                true to "Scripts reloaded"
+            }
+
+            "scripts" -> true to this.pf.scripts.toString()
+
+            else -> true to "Unknown usage"
+        }
+    }
+}
