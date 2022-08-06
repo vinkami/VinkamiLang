eb4033169a9ffb4487e5297cd68922f15c938d31
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-07 05:46:21 +0800

    Fixed: Numbers aren't properly made Fixed: Strings have missing quotes Fixed: Strings aren't detected when found after other tokens Changed: No more this.advanced() after every loop in this.tokenize() Removed: this.rollback() is commented because it's unnecessary

--- src/main/kotlin/com/vinkami/vinkamilang/language/Lexer.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/Lexer.kt
@@ -12,41 +12,82 @@ class Lexer(val text: String) {
         this.current_char = if (this.pos < this.text.length) this.text[this.pos].toString() else null
     }
 
-    private fun rollback() {
-        this.pos--
-        this.current_char = this.text[this.pos].toString()
-    }
+//    private fun rollback() {
+//        this.pos--
+//        this.current_char = this.text[this.pos].toString()
+//    }
 
-    private fun tokenize(): List<Token> {
+    fun tokenize(): List<Token> {
         val tokens: MutableList<Token> = mutableListOf()
 
         while (this.current_char != null) {
             val section =
-                if (Regex("\\d") matches this.current_char!!) {this.makeNumber()}
-                else if (Regex("[a-zA-Z]") matches this.current_char!!) {TODO("Identifier vs String")}
-                else {this.current_char!!}
+                if (Regex("[0-9.]") matches this.current_char!!) {this.makeNumber()}
+                else if (Regex("[a-zA-Z]") matches this.current_char!!) {this.makeIdentifier()}
+                else if (Regex("[\\s\\t\\n\\r]") matches this.current_char!!) {this.makeWhitespace()}
+                else if (Regex("[\'\"]") matches this.current_char!!) {this.makeString()}
+                else {
+                    val c = this.current_char!!
+                    this.advance()
+                    c
+                }
 
             tokens += Token(section)
         }
 
+        // EOF token
+        tokens += Token("EOF")
+
         return tokens
     }
 
-    private fun makeNumber(): String {
+    private fun makeNumber(): String {  // Note: Doesn't care the decimal points' number and placement
         var section = ""
-        var dotCount = 0
 
-        while (Regex("\\d") matches this.current_char.toString()) {
-            section += if (this.current_char == ".") {
-                if (dotCount == 1) {break}
-                dotCount++
-                "."
-            } else {
-                this.current_char
-            }
+        while (this.current_char != null && Regex("[0-9.]") matches this.current_char!!) {
+            section += this.current_char
             this.advance()
         }
-        this.rollback()
+
+        return section
+    }
+
+    private fun makeIdentifier(): String {
+        var section = ""
+
+        while (this.current_char != null && Regex("[a-zA-Z]") matches this.current_char!!) {
+            section += this.current_char
+            this.advance()
+        }
+
+        return section
+    }
+
+    private fun makeWhitespace(): String {
+        var section = ""
+
+        while (this.current_char != null && Regex("[\\s\\t\\n\\r]") matches this.current_char.toString()) {
+            section += this.current_char
+            this.advance()
+        }
+
+        return section
+    }
+
+    private fun makeString(): String {
+        var section = "${this.current_char}"
+        val quote = this.current_char!!
+        this.advance()
+
+        while (this.current_char != null && (this.current_char != quote || if (this.pos != 0) {this.text[this.pos - 1].toString() == "\\"} else true)) {
+            section += this.current_char
+            this.advance()
+        }
+
+        if (this.current_char != null) {
+            section += quote
+            this.advance()  // Current char is the quote, need to advance 1 more time
+        }
 
         return section
     }
