e3f4e146ae671aa0416ca6e4a4885f5e40d135fe
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-07 05:50:53 +0800

    Changed: LESS_THAN -> LESS, GREATER_THAN -> GREATER Remoevd: INT, FLOAT, TAB, EXCLAM Added: NUMBER, INT, AND, OR, NOT, UNKNOWN

--- src/main/kotlin/com/vinkami/vinkamilang/language/TokenType.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/TokenType.kt
@@ -3,18 +3,21 @@ package com.vinkami.vinkamilang.language
 enum class TokenType {
     // Key char
     L_PARAN, R_PARAN, L_BRAC, R_BRAC,
-    COMMA, DOT, COLON, EXCLAM,
+    COMMA, DOT, COLON,
 
     // Operator
     PLUS, MINUS, MULTIPLY, DIVIDE, POWER, MODULO,
-    EQUAL, LESS_THAN, LESS_EQUAL, GREATER_THAN, GREATER_EQUAL, NOT_EQUAL, AND, OR,
+    EQUAL, LESS, LESS_EQUAL, GREATER, GREATER_EQUAL, NOT_EQUAL,
+
+    // LOGIC
+    AND, OR, NOT,
 
     // Keyword
     IF, ELIF, ELSE, TRUE, FALSE, FOR, WHILE, RETURN, VAR, IS, IMPORT, IN,
 
     // Literal
-    IDENTIFIER, INT, FLOAT, STRING,
+    IDENTIFIER, NUMBER, UNKNOWN, STRING,
 
     // Format
-    SPACE, TAB, LINEBREAK, EOF,
+    SPACE, LINEBREAK, EOF,
 }
