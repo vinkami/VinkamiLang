49b4af7284df6ab04c4c52bf30578fd9c199168d
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-10 04:26:10 +0800

    Added: Privacy for basically all variables Added: Position() for all tokens Added: combineTokens() to make >=, ++, SPACE*2, etc. Removed: rollback() because it's not needed Removed: *this.* Removed: makeWhiteSpace() Fixed: current_char -> currentChar Changed: currentChar uses getter instead of relying on advance()

--- src/main/kotlin/com/vinkami/vinkamilang/language/Lexer.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/Lexer.kt
@@ -1,52 +1,53 @@
 package com.vinkami.vinkamilang.language
 
-class Lexer(val text: String) {
-    var pos = -1
-    var current_char: String? = null
-    init {
-        this.advance()
-    }
+class Lexer(private val text: String, private val fileName: String) {
+    private var pos = -1
+    private val currentChar: String?
+        get() = if (pos < text.length) text[pos].toString() else null
+    private var lineNumber: Int = 1
+
+    init {advance()}
 
     private fun advance() {
-        this.pos++
-        this.current_char = if (this.pos < this.text.length) this.text[this.pos].toString() else null
+        pos++
+        if (currentChar == "\n") {
+            lineNumber++
+        }
     }
-
-//    private fun rollback() {
-//        this.pos--
-//        this.current_char = this.text[this.pos].toString()
-//    }
 
     fun tokenize(): List<Token> {
         val tokens: MutableList<Token> = mutableListOf()
 
-        while (this.current_char != null) {
+        while (currentChar != null) {
+            val startPos = pos
+
             val section =
-                if (Regex("[0-9.]") matches this.current_char!!) {this.makeNumber()}
-                else if (Regex("[a-zA-Z]") matches this.current_char!!) {this.makeIdentifier()}
-                else if (Regex("[\\s\\t\\n\\r]") matches this.current_char!!) {this.makeWhitespace()}
-                else if (Regex("[\'\"]") matches this.current_char!!) {this.makeString()}
+                if (Regex("[0-9.]") matches currentChar!!) {makeNumber()}
+                else if (Regex("[a-zA-Z]") matches currentChar!!) {makeIdentifier()}
+                else if (Regex("[\'\"]") matches currentChar!!) {makeString()}
                 else {
-                    val c = this.current_char!!
-                    this.advance()
+                    val c = currentChar!!
+                    advance()
                     c
                 }
 
-            tokens += Token(section)
+            val position = Position(fileName, lineNumber, startPos, pos)
+            tokens += Token(section, position)
         }
 
         // EOF token
-        tokens += Token("EOF")
+        val position = Position(fileName, lineNumber, pos, pos)
+        tokens += Token("EOF", position)
 
-        return tokens
+        return combineTokens(tokens)  // Combine tokens like >= and ++
     }
 
     private fun makeNumber(): String {  // Note: Doesn't care the decimal points' number and placement
         var section = ""
 
-        while (this.current_char != null && Regex("[0-9.]") matches this.current_char!!) {
-            section += this.current_char
-            this.advance()
+        while (currentChar != null && Regex("[0-9.]") matches currentChar!!) {
+            section += currentChar
+            advance()
         }
 
         return section
@@ -55,40 +56,213 @@ class Lexer(val text: String) {
     private fun makeIdentifier(): String {
         var section = ""
 
-        while (this.current_char != null && Regex("[a-zA-Z]") matches this.current_char!!) {
-            section += this.current_char
-            this.advance()
-        }
-
-        return section
-    }
-
-    private fun makeWhitespace(): String {
-        var section = ""
-
-        while (this.current_char != null && Regex("[\\s\\t\\n\\r]") matches this.current_char.toString()) {
-            section += this.current_char
-            this.advance()
+        while (currentChar != null && Regex("[a-zA-Z]") matches currentChar!!) {
+            section += currentChar
+            advance()
         }
 
         return section
     }
 
     private fun makeString(): String {
-        var section = "${this.current_char}"
-        val quote = this.current_char!!
-        this.advance()
+        var section = "${currentChar}"
+        val quote = currentChar!!
+        advance()
 
-        while (this.current_char != null && (this.current_char != quote || if (this.pos != 0) {this.text[this.pos - 1].toString() == "\\"} else true)) {
-            section += this.current_char
-            this.advance()
+        while (currentChar != null && (currentChar != quote || if (pos != 0) {text[pos - 1].toString() == "\\"} else true)) {
+            section += currentChar
+            advance()
         }
 
-        if (this.current_char != null) {
+        if (currentChar != null) {
             section += quote
-            this.advance()  // Current char is the quote, need to advance 1 more time
+            advance()  // Current char is the quote, need to advance 1 more time
         }
 
         return section
+    }
+
+    private fun combineTokens(tokens: MutableList<Token>): MutableList<Token> {
+        // For tokens like >=, ++, etc. they are not combined in the first lexing step, here will do it.
+        // Working principle: loop all tokens, replace it and the next token if combinable, increment i otherwise
+
+        var i = 0
+        var result = tokens
+
+        while (i < tokens.size) {
+            val currentToken = tokens[i]
+            when (currentToken.type) {
+                TokenType.EOF -> {
+                    result = result.dropLast(tokens.size - i - 1) as MutableList<Token>
+                    break
+                }
+
+                TokenType.LESS -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("<=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.GREATER -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token(">=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.ASSIGN -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("==", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.PLUS -> {
+                    when (result[i+1].type) {
+                        TokenType.PLUS -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("++", pos)
+                            result.removeAt(i+1)
+                        }
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("+=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.MINUS -> {
+                    when (result[i+1].type) {
+                        TokenType.MINUS -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("--", pos)
+                            result.removeAt(i+1)
+                        }
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("-=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.MULTIPLY -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("*=", pos)
+                            result.removeAt(i+1)
+                        }
+                        TokenType.MULTIPLY -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("**", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.DIVIDE -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("/=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.POWER -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("**=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.MODULO -> {
+                    when (result[i+1].type) {
+                        TokenType.ASSIGN -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("%=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.NOT -> {
+                    when (result[i+1].type) {
+                        TokenType.EQUAL -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token("!=", pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.SPACE -> {
+                    when (result[i+1].type) {
+                        TokenType.SPACE -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token(currentToken.value + result[i+1].value, pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                TokenType.LINEBREAK -> {
+                    when (result[i+1].type) {
+                        TokenType.LINEBREAK -> {
+                            val ctpos = currentToken.position
+                            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end + 1)
+                            result[i] = Token(currentToken.value + result[i+1].value, pos)
+                            result.removeAt(i+1)
+                        }
+                        else -> {i++}
+                    }
+                }
+
+                else -> {i++}
+            }
+        }
+
+        return result
     }
 }
