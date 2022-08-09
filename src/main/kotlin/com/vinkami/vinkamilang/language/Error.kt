e3ee622fd144dc0f9e1818da681d0f40eae47238
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-10 04:15:21 +0800

    Added

---
+++ src/main/kotlin/com/vinkami/vinkamilang/language/Error.kt
@@ -0,0 +1,19 @@
+package com.vinkami.vinkamilang.language
+
+class LexingException(message: String, private val position: Position): Exception(message) {
+    override fun toString(): String {
+        return """
+            $position
+            LexingException: $message
+        """.trimIndent()
+    }
+}
+
+class ParsingException(message: String, private val position: Position): Exception(message) {
+    override fun toString(): String {
+        return """
+            $position
+            ParsingException: $message
+        """.trimIndent()
+    }
+}
