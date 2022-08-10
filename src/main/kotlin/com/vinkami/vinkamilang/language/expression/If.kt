32b49ab44722616bb16bd7fa4a859978fc49e27b
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-11 02:50:09 +0800

    Added: toString() Added: elif, fallback(else)

--- src/main/kotlin/com/vinkami/vinkamilang/language/expression/If.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/expression/If.kt
@@ -1,7 +1,27 @@
 package com.vinkami.vinkamilang.language.expression
 
-class If(val condition: Expression, val action: Expression): Expression(condition) {
+class If(val condition: Expression, val action: Expression, val elif: Map<Expression, Expression>, val fallback: Expression): Expression(condition) {
   override fun toString(): String {
-    return "(if ($condition) {$action})"
+    // Structure: (if (condition) -> (action) | (condition) -> (action) | (fallback))
+    val sb = StringBuilder()
+    sb.append("(if ")
+      .append(condition)
+      .append(" -> ")
+      .append(action)
+
+    for ((elifCondition, elifAction) in elif) {
+      sb.append(" | ")
+        .append(elifCondition)
+        .append(" -> ")
+        .append(elifAction)
+    }
+
+    if (fallback !is Null) {
+      sb.append(" | (")
+        .append(fallback)
+        .append(")")
+    }
+    sb.append(")")
+    return sb.toString()
   }
 }
