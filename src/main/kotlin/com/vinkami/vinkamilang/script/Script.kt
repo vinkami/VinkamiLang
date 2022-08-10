c6a6a47b706dfcab40feca67d9da0efc76646666
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-11 03:03:42 +0800

    Changed: toString() to indicate itself is a script Removed: privacy for this.code, this.name  # Why are they private in the first place? Removed: *this.* Removed: this.file, this.pf. They don't serve any purpose after being translated to this.code and this.name

--- src/main/kotlin/com/vinkami/vinkamilang/script/Script.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/script/Script.kt
@@ -3,12 +3,12 @@ package com.vinkami.vinkamilang.script
 import com.vinkami.vinkamilang.PathFinder
 import java.io.File
 
-class Script(private val file: File, private var pf: PathFinder) {
-    private var code = ""
-    private val name = this.file.relativeTo(this.pf.plugin.dataFolder.resolve("scripts"))
-                           .toString()
-                           .replace("\\", ".")
-                           .replace(".vk", "")
+class Script(file: File, pf: PathFinder) {
+    var code = ""
+    val name = file.relativeTo(pf.plugin.dataFolder.resolve("scripts"))
+                   .toString()
+                   .replace("\\", ".")
+                   .replace(".vk", "")
     init {
         file.forEachLine {
             code += it + "\n"
@@ -16,6 +16,6 @@ class Script(private val file: File, private var pf: PathFinder) {
     }
 
     override fun toString(): String {
-        return this.name
+        return "<Script $name>"
     }
 }
