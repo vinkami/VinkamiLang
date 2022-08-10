4312ec4eef9106bb8914dfe113f49349e3377cd9
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-11 02:51:53 +0800

    Renamed. It handles comparisons so Arithmetic.kt doesn't match the usage

--- src/main/kotlin/com/vinkami/vinkamilang/language/expression/Math.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/expression/Math.kt
@@ -2,7 +2,7 @@ package com.vinkami.vinkamilang.language.expression
 
 import com.vinkami.vinkamilang.language.Token
 
-class Arithmetic(op: Token, val lhs: Expression, val rhs: Expression): Expression(op) {
+class Math(op: Token, val lhs: Expression, val rhs: Expression): Expression(op) {
     val op = value
 
     override fun toString(): String {
