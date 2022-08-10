1fe779ae5139f4e54e19192e3daf8d7a666d4c16
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-11 02:47:08 +0800

    Added: Binding power of pratt parsing from exprMath()

--- src/main/kotlin/com/vinkami/vinkamilang/language/Constant.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/Constant.kt
@@ -30,4 +30,15 @@ object Constant {
         TokenType.L_BRAC to TokenType.R_BRAC,
         TokenType.L_BRACE to TokenType.R_BRACE,
     )
+
+    val bindingPower: Map<TokenType, Pair<Int, Int>> = mapOf(
+        TokenType.PLUS to Pair(4, 5), TokenType.MINUS to Pair(4, 5),
+        TokenType.MULTIPLY to Pair(6, 7), TokenType.DIVIDE to Pair(6, 7),
+        TokenType.MODULO to Pair(8, 9),
+        TokenType.POWER to Pair(10, 11),
+
+        TokenType.EQUAL to Pair(2, 3), TokenType.NOT_EQUAL to Pair(2, 3),
+        TokenType.LESS to Pair(2, 3), TokenType.LESS_EQUAL to Pair(2, 3),
+        TokenType.GREATER to Pair(2, 3), TokenType.GREATER_EQUAL to Pair(2, 3),
+    )
 }
