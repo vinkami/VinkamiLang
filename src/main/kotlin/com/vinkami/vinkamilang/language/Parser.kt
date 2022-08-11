4d6c0af16e708b3a79b0289c9f7acc88e6e75387
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-08-12 02:39:18 +0800

    Added: aas() for quick advance() skipSpace() Added: exprWhile() Added: gotoNext(TT) to navigate and skip irrelevant tokens Removed: captureTokensAndParse() in exprIf(). exprBracket() is a better alternative

--- src/main/kotlin/com/vinkami/vinkamilang/language/Parser.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/Parser.kt
@@ -2,7 +2,8 @@ package com.vinkami.vinkamilang.language
 
 import com.vinkami.vinkamilang.language.exception.ParsingException
 import com.vinkami.vinkamilang.language.expression.*
-import com.vinkami.vinkamilang.language.expression.Number
+import com.vinkami.vinkamilang.language.position.LexingPosition
+import com.vinkami.vinkamilang.language.position.ParsingPosition
 
 
 class Parser(private val tokens: List<Token>) {
@@ -33,17 +34,32 @@ class Parser(private val tokens: List<Token>) {
         while (listOf(TokenType.SPACE, TokenType.LINEBREAK).contains(currentToken.type)) advance()
     }
 
+    private fun aas(){
+        advance()
+        skipSpace()
+    }
+
     fun parse(): Expression {
         skipSpace()
         val expr = when (currentToken.type) {
             TokenType.NUMBER, TokenType.L_PARAN, TokenType.IDENTIFIER -> exprMath(0)
             TokenType.IF -> exprIf()
+            TokenType.WHILE -> exprWhile()
             TokenType.EOF -> Null()
             else -> throw ParsingException("Unexpected token $currentToken", currentToken.position)
             // TODO("Other expr types not implemented")
         }
 
         return expr
+    }
+
+    private fun gotoNext(TT: TokenType){
+        while (currentToken.type != TT) {
+            if (currentToken.type == TokenType.EOF) {
+                throw ParsingException("Script ended when expecting a $TT", currentToken.position)
+            }
+            advance()
+        }
     }
 
     private fun exprMath(minBP: Int): Expression {
@@ -53,7 +69,9 @@ class Parser(private val tokens: List<Token>) {
         var lhs: Expression = if (currentToken.type == TokenType.L_PARAN) {
             exprBracket()
         } else {
-            Number(currentToken)
+            val ctpos = currentToken.position
+            val ppos = ParsingPosition(pos, pos, ctpos, ctpos)
+            Number(currentToken, ppos)
         }
 
         while (true) {
@@ -62,11 +80,12 @@ class Parser(private val tokens: List<Token>) {
 
             val (leftBP, rightBP) = Constant.bindingPower[op.type]!!
             if (leftBP < minBP) {break}
-            advance()
-            skipSpace()
+            aas()
 
             val rhs = exprMath(rightBP)
-            lhs = Math(op, lhs, rhs)
+            val ppos = ParsingPosition(lhs.position.start, rhs.position.end,
+                                       lhs.position.startTokenPosition, rhs.position.endTokenPosition)
+            lhs = Math(op, lhs, rhs, ppos)
         }
         return lhs
     }
@@ -77,74 +96,104 @@ class Parser(private val tokens: List<Token>) {
         var paranCount = 1
         val start = pos
         val startToken = currentToken
-        while (paranCount > 0) {
+
+        while (paranCount > 0) {  // Find the matching closing bracket in terms of number
             advance()
             val tt = currentToken.type
             if (tt == TokenType.EOF) {throw ParsingException("Script ended with unclosed bracket", startToken.position)}
             else if (Constant.bracket.keys.contains(tt)) {paranCount++}
             else if (Constant.bracket.values.contains(tt)) {paranCount--}
         }
-        if (currentToken.type != bracketTypeR) {
+
+        if (currentToken.type != bracketTypeR) {  // Confirm the "matching" bracket is the same type
             val ctpos = currentToken.position
-            val pos = Position(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end)
+            val pos = LexingPosition(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end)
             throw ParsingException("Expected closing bracket ${bracketTypeR.name} and found ${currentToken.type}", pos)
         }
+
         val end = pos
         val endToken = currentToken
-        val eofPosition = Position(endToken.position.filename, endToken.position.lineNumber,
-                              endToken.position.end + 1, endToken.position.end + 1)
+        val eofPosition = LexingPosition(endToken.position.filename, endToken.position.lineNumber,
+                                    endToken.position.end - 1, endToken.position.end - 1)
+
         val innerTokens = tokens.subList(start + 1, end) + Token("EOF", eofPosition)
+
         val innerExpr = if (innerTokens.isNotEmpty()) Parser(innerTokens).parse() else Null()
-        return Bracket(innerExpr, startToken, endToken)
+
+        val ppos = ParsingPosition(start, end, startToken.position, endToken.position)
+        return Bracket(innerExpr, startToken, endToken, ppos)
     }
 
     private fun exprIf(): Expression {
-        /**
-         * Make sure this.pos points to startToken.position, otherwise the subList will be wrong
-         * This function will advance pos to exactly where endTT is at
-         *
-         * startPos: position of the TT.IF, TT.ELIF for condition; TT.L_BRACE for action
-         * endTT: TT.L_BRACE for condition; TT.R_BRACE for action
-         */
-        fun captureTokensAndParse(endTT: TokenType): Expression {
-            val startPos = currentToken.position
-            skipSpace()
-            val start = pos
-            while (currentToken.type != endTT) {
-                if (currentToken.type == TokenType.EOF) {
-                    throw ParsingException("Script ended with unclosed if statement", startPos)
-                }
-                advance()
-            }
+        val start = pos
+        val startToken = currentToken
 
-            val eofPosition = Position(currentToken.position.filename, currentToken.position.lineNumber,
-                              currentToken.position.end + 1, currentToken.position.end + 1)
-            val tokenSection = tokens.subList(start + 1, pos) + Token("EOF", eofPosition)
-            return Parser(tokenSection).parse()
-        }
-
-        val mainCondition = captureTokensAndParse(TokenType.L_BRACE)  // if ... {
-        val mainAction = captureTokensAndParse(TokenType.R_BRACE)  // { ... }
+        aas()
+        val mainCondition = exprBracket()  // if (...) {
+        aas()
+        val mainAction = exprBracket()  // { ... }
 
         val elif = mutableMapOf<Expression, Expression>()
-        advance()
-        skipSpace()
+        aas()
         while (currentToken.type == TokenType.ELIF) {
-            val condition = captureTokensAndParse(TokenType.L_BRACE)
-            val action = captureTokensAndParse(TokenType.R_BRACE)
+            val condition = exprBracket()  // elif (...) {
+            aas()
+            val action = exprBracket()  // { ... }
             elif[condition] = action
-            advance()
-            skipSpace()
+            aas()
         }
 
         var elseAction: Expression = Null()
         if (currentToken.type == TokenType.ELSE) {
-            while (currentToken.type != TokenType.L_BRACE) {advance()}
-            elseAction = captureTokensAndParse(TokenType.R_BRACE)
-            advance()
-            skipSpace()
+            gotoNext(TokenType.L_BRACE)
+            elseAction = exprBracket()  // { ... }
+            aas()
         }
 
-        return If(mainCondition, mainAction, elif, elseAction)
+        val end = pos
+        val endToken = currentToken
+
+        val ppos = ParsingPosition(start, end, startToken.position, endToken.position)
+        return If(mainCondition, mainAction, elif, elseAction, ppos)
     }
+
+    private fun exprWhile(): Expression {
+        val start = pos
+        val startToken = currentToken
+
+        aas()
+        val condition = exprBracket()  // while (...) {
+        aas()
+        val mainAction = exprBracket()  // { ... }
+
+        aas()
+        var completeAction: Expression = Null()
+        var incompleteAction: Expression = Null()
+        while (listOf(TokenType.COMPLETE, TokenType.INCOMPLETE).contains(currentToken.type)) {
+            val tokenType = currentToken.type
+            gotoNext(TokenType.L_BRACE)
+            val action = exprBracket()  // { ... }
+            if (tokenType == TokenType.COMPLETE) {
+                if (completeAction !is Null) throw ParsingException("While block following with multiple complete actions", currentToken.position)
+                completeAction = action
+            } else {
+                if (incompleteAction !is Null) throw ParsingException("While block following with multiple incomplete actions", currentToken.position)
+                incompleteAction = action
+            }
+
+            aas()
+        }
+
+        val end = pos
+        val endToken = currentToken
+
+        val ppos = ParsingPosition(start, end, startToken.position, endToken.position)
+        return While(condition, mainAction, completeAction, incompleteAction, ppos)
+    }
+
+    private fun exprFor(): Expression {
+        TODO("Not yet implemented")
+    }
+
+
 }
