358688a8eae591ad9b0e7d5fe795e7b8d37bb01b
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-09-25 20:23:43 +0800

    Added: Position Added: Operations

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/NumberObj.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/NumberObj.kt
@@ -1,7 +1,43 @@
 package com.vinkami.vinkamilang.language.interpret.`object`
 
-class NumberObj(override val value: Float): BaseObject {
+import com.vinkami.vinkamilang.language.lex.Position
+import kotlin.math.pow
+
+class NumberObj(override val value: Float, override val startPos: Position, override val endPos: Position): BaseObject {
     override fun toString(): String {
         return value.toString()
+    }
+
+    override operator fun plus(other: BaseObject): BaseObject {
+        if (other is NumberObj) return NumberObj(value + other.value, startPos, other.endPos)
+        return super.plus(other)
+    }
+
+    override operator fun minus(other: BaseObject): BaseObject {
+        if (other is NumberObj) return NumberObj(value - other.value, startPos, other.endPos)
+        return super.minus(other)
+    }
+
+    override operator fun times(other: BaseObject): BaseObject {
+        if (other is NumberObj) return NumberObj(value * other.value, startPos, other.endPos)
+        return super.times(other)
+    }
+
+    override operator fun div(other: BaseObject): BaseObject {
+        if (other is NumberObj) return NumberObj(value / other.value, startPos, other.endPos)
+        return super.div(other)
+    }
+
+    override fun power(other: BaseObject): BaseObject {
+        if (other is NumberObj) return NumberObj(value.pow(other.value), startPos, other.endPos)
+        return super.power(other)
+    }
+
+    override operator fun unaryPlus(): BaseObject {
+        return NumberObj(+value, startPos, endPos)
+    }
+
+    override operator fun unaryMinus(): BaseObject {
+        return NumberObj(-value, startPos, endPos)
     }
 }
