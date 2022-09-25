fe87c12a340ac45c493c47970aa5eeaed8a1e021
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-09-25 20:26:33 +0800

    Added: More operations (Unary, Null, Binary modulo, Binary power)

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/Interpreter.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/Interpreter.kt
@@ -2,12 +2,10 @@ package com.vinkami.vinkamilang.language.interpret
 
 import com.vinkami.vinkamilang.language.exception.SyntaxError
 import com.vinkami.vinkamilang.language.exception.UnknownNodeError
+import com.vinkami.vinkamilang.language.interpret.`object`.NullObj
 import com.vinkami.vinkamilang.language.interpret.`object`.NumberObj
 import com.vinkami.vinkamilang.language.lex.TokenType
-import com.vinkami.vinkamilang.language.parse.node.BaseNode
-import com.vinkami.vinkamilang.language.parse.node.BinOpNode
-import com.vinkami.vinkamilang.language.parse.node.BracketNode
-import com.vinkami.vinkamilang.language.parse.node.NumberNode
+import com.vinkami.vinkamilang.language.parse.node.*
 
 class Interpreter(val node: BaseNode) {
     fun interpret(): InterpretResult = interpret(node)
@@ -18,7 +16,9 @@ class Interpreter(val node: BaseNode) {
         return when (node) {
             is NumberNode -> interpretNumber(node)
             is BinOpNode -> interpretBinOp(node)
+            is UnaryOpNode -> interpretUnaryOp(node)
             is BracketNode -> interpretBracket(node)
+            is NullNode -> InterpretResult(NullObj(node.startPos, node.endPos))
             else -> InterpretResult(UnknownNodeError(node))
         }
     }
@@ -27,7 +27,7 @@ class Interpreter(val node: BaseNode) {
         val valueString = node.tok.value
         return try {
             val value = valueString.toFloat()
-            InterpretResult(NumberObj(value))
+            InterpretResult(NumberObj(value, node.startPos, node.endPos))
         } catch (e: NumberFormatException) {
             InterpretResult(SyntaxError("Invalid number: $valueString", node.startPos, node.endPos))
         }
@@ -36,20 +36,34 @@ class Interpreter(val node: BaseNode) {
     private fun interpretBinOp(node: BinOpNode): InterpretResult {
         val res = InterpretResult()
 
-        val left = res(interpret(node.left)).obj as NumberObj
-        val right = res(interpret(node.right)).obj as NumberObj
-        res.hasError && return res
+        val left = res(interpret(node.left)).also{res.hasError && return res}.obj
+        val right = res(interpret(node.right)).also{res.hasError && return res}.obj
+
 
         return when (node.op.type) {
-            TokenType.PLUS -> res(NumberObj(left.value + right.value))
-            TokenType.MINUS -> res(NumberObj(left.value - right.value))
-            TokenType.MULTIPLY -> res(NumberObj(left.value * right.value))
-            TokenType.DIVIDE -> res(NumberObj(left.value / right.value))
+            TokenType.PLUS -> res(left + right)
+            TokenType.MINUS -> res(left - right)
+            TokenType.MULTIPLY -> res(left * right)
+            TokenType.DIVIDE -> res(left / right)
+            TokenType.MODULO -> res(left % right)
+            TokenType.POWER -> res(left.power(right))
+            else -> res(UnknownNodeError(node))
+        }
+    }
+
+    private fun interpretUnaryOp(node: UnaryOpNode): InterpretResult {
+        val res = InterpretResult()
+
+        val innerNode = res(interpret(node.innerNode)).also{res.hasError && return res}.obj
+
+        return when (node.op.type) {
+            TokenType.PLUS -> res(+innerNode)
+            TokenType.MINUS -> res(-innerNode)
             else -> res(UnknownNodeError(node))
         }
     }
 
     private fun interpretBracket(node: BracketNode): InterpretResult {
-        return interpret(node.node)
+        return interpret(node.innerNode)
     }
 }
