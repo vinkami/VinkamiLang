251cb377ccd287765dfb72b89aa8ffef4c5bbdae
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-09-25 20:24:35 +0800

    Added: Unary operations Removed: Identifier to trigger binary operations. TODO variables

--- src/main/kotlin/com/vinkami/vinkamilang/language/parse/Parser.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/parse/Parser.kt
@@ -51,7 +51,8 @@ class Parser(private val tokens: List<Token>) {
         skipSpace().let { if (it != null) return ParseResult(it) }
 
         return when (currentToken.type) {
-            TokenType.NUMBER, TokenType.L_PARAN, TokenType.IDENTIFIER -> parseMath(0)
+            TokenType.NUMBER, TokenType.L_PARAN -> parseBinOp(0)
+            TokenType.PLUS, TokenType.MINUS -> parseUnaryOp()
 //            TokenType.IF -> stmIf()
 //            TokenType.WHILE, TokenType.FOR -> stmLoop()
             TokenType.EOF -> ParseResult(NullNode(currentToken))
@@ -71,13 +72,14 @@ class Parser(private val tokens: List<Token>) {
 //        return null
 //    }
 
-    private fun parseMath(minBP: Int): ParseResult {
+    private fun parseBinOp(minBP: Int): ParseResult {
         val res = ParseResult()
         if (minBP != 0) res(advance())
         res(skipSpace())
 
         var lhs: BaseNode = if (currentToken.type == TokenType.L_PARAN) {
-            val brac = parseBracket()
+            val brac = res(parseBracket())
+            res.hasError && return res
             brac.node
         } else {
             NumberNode(currentToken)
@@ -91,12 +93,21 @@ class Parser(private val tokens: List<Token>) {
             if (leftBP < minBP) {break}
             res(ass())
 
-            val rhs = parseMath(rightBP)
+            val rhs = res(parseBinOp(rightBP))
+            res.hasError && return res
             lhs = BinOpNode(lhs, op, rhs.node)
         }
         return res(lhs)
     }
 
+    private fun parseUnaryOp(): ParseResult {
+        val res = ParseResult()
+        val op = currentToken
+        res(ass())
+        val node = res(parse())
+        res.hasError && return res
+        return res(UnaryOpNode(op, node.node))
+    }
 
     private fun parseBracket(): ParseResult {
         val res = ParseResult()
@@ -110,7 +121,7 @@ class Parser(private val tokens: List<Token>) {
         while (paranCount > 0) {  // Find the matching closing bracket in terms of number
             res(advance())
             val tt = currentToken.type
-            if (tt == TokenType.EOF) throw SyntaxError("Script ended when expecting a $bracketTypeR", currentToken.startPos, currentToken.endPos)
+            if (tt == TokenType.EOF) return res(SyntaxError("Script ended when expecting a $bracketTypeR", currentToken.startPos, currentToken.endPos))
             else if (Constant.bracket.keys.contains(tt)) paranCount++
             else if (Constant.bracket.values.contains(tt)) paranCount--
         }
@@ -123,80 +134,12 @@ class Parser(private val tokens: List<Token>) {
         val innerTokens = tokens.subList(start + 1, pos) + eof
         val innerResult = res(if (innerTokens.isNotEmpty()) Parser(innerTokens).parse() else ParseResult(NullNode(currentToken)))
 
+        res.hasError && return res
         return res(BracketNode(startToken, innerResult.node, endToken))
     }
 
 
-//    private fun exprMath(minBP: Int): BaseExpression {
-//        if (minBP != 0) {advance()}  // Advance first if it's called by itself
-//        skipSpace()
-//
-//        var lhs: BaseExpression = if (currentToken.type == TokenType.L_PARAN) {
-//            exprBracket()
-//        } else {
-//            val ctpos = currentToken.position
-//            val ppos = ParsingPosition(pos, pos, ctpos, ctpos)
-//            NumberExpr(currentToken, ppos)
-//        }
-//
-//        while (true) {
-//            val op = nextNonSpaceToken()
-//            if (!(Constant.arithmeticOp + Constant.comparitiveOp).contains(op.type)) {break}
-//
-//            val (leftBP, rightBP) = Constant.bindingPower[op.type]!!
-//            if (leftBP < minBP) {break}
-//            ass()
-//
-//            val rhs = exprMath(rightBP)
-//            val ppos = ParsingPosition(lhs.position.start, rhs.position.end,
-//                                       lhs.position.startTokenPosition, rhs.position.endTokenPosition)
-//            lhs = MathExpr(op, lhs, rhs, ppos)
-//        }
-//        return lhs
-//    }
-//
-//    private fun stmBracket(): BracketStm {
-//        val bracketTypeL = currentToken.type
-//        val bracketTypeR = Constant.bracket[bracketTypeL]!!
-//        var paranCount = 1
-//        val start = pos
-//        val startToken = currentToken
-//
-//        while (paranCount > 0) {  // Find the matching closing bracket in terms of number
-//            advance()
-//            val tt = currentToken.type
-//            if (tt == TokenType.EOF) {throw ParsingException("Script ended with unclosed bracket", startToken.position)}
-//            else if (Constant.bracket.keys.contains(tt)) {paranCount++}
-//            else if (Constant.bracket.values.contains(tt)) {paranCount--}
-//        }
-//
-//        if (currentToken.type != bracketTypeR) {  // Confirm the "matching" bracket is the same type
-//            val ctpos = currentToken.position
-//            val pos = LexingPosition(ctpos.filename, ctpos.lineNumber, ctpos.start, ctpos.end)
-//            throw ParsingException("Expected closing bracket ${bracketTypeR.name} and found ${currentToken.type}", pos)
-//        }
-//
-//        val end = pos
-//        val endToken = currentToken
-//        val eofPosition = LexingPosition(endToken.position.filename, endToken.position.lineNumber,
-//            endToken.position.end - 1, endToken.position.end - 1)
-//
-//        val innerTokens = tokens.subList(start + 1, end) + Token("EOF", eofPosition)
-//
-//        val innerStm = if (innerTokens.isNotEmpty()) Parser(innerTokens).parse() else NullExpr()
-//
-//        val ppos = ParsingPosition(start, end, startToken.position, endToken.position)
-//        return BracketStm(innerStm, startToken, endToken, ppos)
-//    }
-//
-//    private fun exprBracket(): BaseExpression {
-//        val bracStm = stmBracket()
-//
-//        if (bracStm.stm !is BaseExpression) { throw ParsingException("Expected expression inside bracket", bracStm.position.startTokenPosition) }
-//
-//        return BracketExpr(bracStm.stm, bracStm.bracL, bracStm.bracR, bracStm.position)
-//    }
-//
+
 //    private fun stmIf(): IfStm {
 //        val start = pos
 //        val startToken = currentToken
