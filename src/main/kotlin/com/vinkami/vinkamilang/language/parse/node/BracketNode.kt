13a4cef06049337338a8c7782c043b6c1a3bdade
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-09-25 20:25:00 +0800

    Changed: Renamed node to innerNode for better reading

--- src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/BracketNode.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/BracketNode.kt
@@ -2,8 +2,8 @@ package com.vinkami.vinkamilang.language.parse.node
 
 import com.vinkami.vinkamilang.language.lex.Token
 
-class BracketNode(val left: Token, val node: BaseNode, val right: Token): BaseNode(left.startPos, right.endPos) {
+class BracketNode(val left: Token, val innerNode: BaseNode, val right: Token): BaseNode(left.startPos, right.endPos) {
     override fun toString(): String {
-        return "($left $node $right)"
+        return "($left $innerNode $right)"
     }
 }
