67675da0ded67758394c017512aeedd481c8ebdb
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-09-29 00:57:18 +0800

    Removed: Unnecessary

--- src/main/kotlin/com/vinkami/vinkamilang/language/exception/DebugMessage.kt
+++
@@ -1,5 +0,0 @@
-package com.vinkami.vinkamilang.language.exception
-
-import com.vinkami.vinkamilang.language.lex.Position
-
-class DebugMessage(message: String, pos: Position): BaseLangException(message, pos, pos)
