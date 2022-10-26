cf92d290492ead91935e75331e462b0fbab47f9c
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-10-26 17:04:26 +0800

    Consistency changes for object functions Changed: Operator fun -> Normal fun

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/BaseObject.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/BaseObject.kt
@@ -9,15 +9,15 @@ interface BaseObject {
     val endPos: Position
     fun boolVal(): Boolean = true
 
-    operator fun plus(other: BaseObject): BaseObject = throw SyntaxError("Can't add $this and $other", startPos, endPos)
-    operator fun minus(other: BaseObject): BaseObject = throw SyntaxError("Can't subtract $this by $other", startPos, endPos)
-    operator fun times(other: BaseObject): BaseObject = throw SyntaxError("Can't multiply $this and $other", startPos, endPos)
-    operator fun div(other: BaseObject): BaseObject = throw SyntaxError("Can't divide $this by $other", startPos, endPos)
-    operator fun rem(other: BaseObject): BaseObject = throw SyntaxError("Can't get remainder of $this divided by $other", startPos, endPos)
+    fun plus(other: BaseObject): BaseObject = throw SyntaxError("Can't add $this and $other", startPos, endPos)
+    fun minus(other: BaseObject): BaseObject = throw SyntaxError("Can't subtract $this by $other", startPos, endPos)
+    fun times(other: BaseObject): BaseObject = throw SyntaxError("Can't multiply $this and $other", startPos, endPos)
+    fun divide(other: BaseObject): BaseObject = throw SyntaxError("Can't divide $this by $other", startPos, endPos)
+    fun mod(other: BaseObject): BaseObject = throw SyntaxError("Can't get remainder of $this divided by $other", startPos, endPos)
     fun power(other: BaseObject): BaseObject = throw SyntaxError("Can't get $this to the power of $other", startPos, endPos)
 
-    operator fun unaryPlus(): BaseObject = throw SyntaxError("Can't apply unary plus to $this", startPos, endPos)
-    operator fun unaryMinus(): BaseObject = throw SyntaxError("Can't apply unary minus to $this", startPos, endPos)
+    fun unaryPlus(): BaseObject = throw SyntaxError("Can't apply unary plus to $this", startPos, endPos)
+    fun unaryMinus(): BaseObject = throw SyntaxError("Can't apply unary minus to $this", startPos, endPos)
 
     fun equal(other: BaseObject): BaseObject = BoolObj(value == other.value, startPos, other.endPos)
     fun notEqual(other: BaseObject): BaseObject = BoolObj(value != other.value, startPos, other.endPos)
