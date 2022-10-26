1a6636f2c37df15ad8dc15163e62e4ecda758ce2
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-10-26 17:08:43 +0800

    Consistency changes for object functions

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/NumberObj.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/NumberObj.kt
@@ -1,6 +1,5 @@
 package com.vinkami.vinkamilang.language.interpret.`object`
 
-import com.vinkami.vinkamilang.language.exception.SyntaxError
 import com.vinkami.vinkamilang.language.lex.Position
 import kotlin.math.pow
 
@@ -11,24 +10,24 @@ class NumberObj(override val value: Float, override val startPos: Position, over
         return value.toString()
     }
 
-    override operator fun plus(other: BaseObject): BaseObject {
+    override  fun plus(other: BaseObject): BaseObject {
         if (other is NumberObj) return NumberObj(value + other.value, startPos, other.endPos)
         return super.plus(other)
     }
 
-    override operator fun minus(other: BaseObject): BaseObject {
+    override  fun minus(other: BaseObject): BaseObject {
         if (other is NumberObj) return NumberObj(value - other.value, startPos, other.endPos)
         return super.minus(other)
     }
 
-    override operator fun times(other: BaseObject): BaseObject {
+    override  fun times(other: BaseObject): BaseObject {
         if (other is NumberObj) return NumberObj(value * other.value, startPos, other.endPos)
         return super.times(other)
     }
 
-    override operator fun div(other: BaseObject): BaseObject {
+    override fun divide(other: BaseObject): BaseObject {
         if (other is NumberObj) return NumberObj(value / other.value, startPos, other.endPos)
-        return super.div(other)
+        return super.divide(other)
     }
 
     override fun power(other: BaseObject): BaseObject {
@@ -36,31 +35,31 @@ class NumberObj(override val value: Float, override val startPos: Position, over
         return super.power(other)
     }
 
-    override operator fun unaryPlus(): BaseObject {
+    override  fun unaryPlus(): BaseObject {
         return NumberObj(+value, startPos, endPos)
     }
 
-    override operator fun unaryMinus(): BaseObject {
+    override  fun unaryMinus(): BaseObject {
         return NumberObj(-value, startPos, endPos)
     }
 
     override fun lessEqual(other: BaseObject): BaseObject {
-        if (other !is NumberObj) throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
-        return BoolObj(value <= other.value, startPos, other.endPos)
+        if (other is NumberObj) return BoolObj(value <= other.value, startPos, other.endPos)
+        return super.lessEqual(other)
     }
 
     override fun greaterEqual(other: BaseObject): BaseObject {
-        if (other !is NumberObj) throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
-        return BoolObj(value >= other.value, startPos, other.endPos)
+        if (other is NumberObj) return BoolObj(value >= other.value, startPos, other.endPos)
+        return super.greaterEqual(other)
     }
 
     override fun less(other: BaseObject): BaseObject {
-        if (other !is NumberObj) throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
-        return BoolObj(value < other.value, startPos, other.endPos)
+        if (other is NumberObj) return BoolObj(value < other.value, startPos, other.endPos)
+        return super.less(other)
     }
 
     override fun greater(other: BaseObject): BaseObject {
-        if (other !is NumberObj) throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
-        return BoolObj(value > other.value, startPos, other.endPos)
+        if (other is NumberObj) return BoolObj(value > other.value, startPos, other.endPos)
+        return super.greater(other)
     }
 }
