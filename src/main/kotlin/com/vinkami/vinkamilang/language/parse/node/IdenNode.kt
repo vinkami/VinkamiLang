94c11a716095879d7ecca38ad1e8463fcff774ed
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-12-16 01:20:41 +0800

    IdenNode now includes information for being called Added: IdenNode.args, .kwargs. .withCall Changed: IdenNode.toString()

--- src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/IdenNode.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/IdenNode.kt
@@ -1,11 +1,15 @@
 package com.vinkami.vinkamilang.language.parse.node
 
+import com.vinkami.vinkamilang.language.lex.Position
 import com.vinkami.vinkamilang.language.lex.Token
 
-class IdenNode(token: Token): BaseNode(token.startPos, token.endPos) {
-    val name = token.value
+class IdenNode(name: Token, val args: List<BaseNode>, val kwargs: Map<Token, BaseNode>, val withCall: Boolean, endPos: Position): BaseNode(name.startPos, endPos) {
+    val name = name.value
 
     override fun toString(): String {
-        return name
+        val sb = StringBuilder()
+        sb.append(name)
+        if (withCall) sb.append("(args: $args; kwargs: $kwargs)")
+        return sb.toString()
     }
 }
