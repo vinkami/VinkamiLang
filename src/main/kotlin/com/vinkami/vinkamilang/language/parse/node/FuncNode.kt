50decd0fa699cb249c186117ebb2feea17539d0e
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-12-16 01:21:53 +0800

    Reduce redundancy in FuncNode Changed: Use Token for name instead of IdenNode

--- src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/FuncNode.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/FuncNode.kt
@@ -1,8 +1,9 @@
 package com.vinkami.vinkamilang.language.parse.node
 
 import com.vinkami.vinkamilang.language.lex.Position
+import com.vinkami.vinkamilang.language.lex.Token
 
-class FuncNode(val name: IdenNode, val params: List<ParamNode>, val returnType: IdenNode?, val body: BaseNode, startPos: Position, endPos: Position): BaseNode(startPos, endPos) {
+class FuncNode(val name: Token, val params: List<ParamNode>, val returnType: IdenNode?, val body: BaseNode, startPos: Position, endPos: Position): BaseNode(startPos, endPos) {
     override fun toString(): String {
         val sb = StringBuilder()
         sb.append("(fun $name(")
