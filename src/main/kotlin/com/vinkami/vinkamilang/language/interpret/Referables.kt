b720541375fdbb93caeeff215caf0a921c7245fb
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-04-01 04:20:49 +0800

    Added: stdout() parameter Added: isRoot parameter Added: builtin function print()

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/Referables.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/Referables.kt
@@ -1,9 +1,16 @@
 package com.vinkami.vinkamilang.language.interpret
 
 import com.vinkami.vinkamilang.language.interpret.`object`.BaseObject
+import com.vinkami.vinkamilang.language.interpret.`object`.builtin.PrintFunc
 
-data class Referables(val variables: MutableMap<String, BaseObject> = mutableMapOf()) {
-    var parent: Referables? = null
+data class Referables(var stdout: ((String)-> Unit)?, val variables: MutableMap<String, BaseObject> = mutableMapOf(), val isRoot: Boolean=true) {
+    private var parent: Referables? = null
+
+    init {
+        if (isRoot) {
+            set("print", PrintFunc())
+        }
+    }
 
     fun get(name: String): BaseObject? {
         return variables[name] ?: parent?.get(name)
@@ -19,7 +26,7 @@ data class Referables(val variables: MutableMap<String, BaseObject> = mutableMap
     }
 
     fun bornChild(): Referables {
-        val child = Referables()
+        val child = Referables(stdout, isRoot=false)
         child.parent = this
         return child
     }
