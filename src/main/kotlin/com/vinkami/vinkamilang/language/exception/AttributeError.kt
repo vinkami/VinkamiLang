83e73adb347e3a18a7940397761f5d0bde0c26b8
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-04-16 04:25:37 +0800

    Created

---
+++ src/main/kotlin/com/vinkami/vinkamilang/language/exception/AttributeError.kt
@@ -0,0 +1,5 @@
+package com.vinkami.vinkamilang.language.exception
+
+import com.vinkami.vinkamilang.language.lex.Position
+
+class AttributeError(message: String, startPos: Position, endPos: Position): BaseError(message, startPos, endPos)
