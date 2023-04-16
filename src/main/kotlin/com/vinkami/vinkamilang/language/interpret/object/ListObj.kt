4a741a146d9616748389cc85c39f3f85ed2545ee
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-04-17 05:51:57 +0800

    Created

---
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/ListObj.kt
@@ -0,0 +1,12 @@
+package com.vinkami.vinkamilang.language.interpret.`object`
+
+import com.vinkami.vinkamilang.language.interpret.Referables
+import com.vinkami.vinkamilang.language.lex.Position
+
+class ListObj(val elements: List<BaseObject>, override val startPos: Position, override val endPos: Position): BaseObject {
+    override val type = "Array"
+    override val value = elements.joinToString(", ", "[", "]")
+    override val property = Referables(null, isRoot=false)
+    override fun boolVal() = elements.isNotEmpty()
+    override fun toString() = value
+}
---
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/MapObj.kt
@@ -0,0 +1,12 @@
+package com.vinkami.vinkamilang.language.interpret.`object`
+
+import com.vinkami.vinkamilang.language.interpret.Referables
+import com.vinkami.vinkamilang.language.lex.Position
+
+class MapObj(val elements: Map<String, BaseObject>, override val startPos: Position, override val endPos: Position): BaseObject {
+    override val type = "Map"
+    override val value = elements.entries.joinToString(", ", "{", "}") { "${it.key}: ${it.value}" }
+    override val property = Referables(null, isRoot=false)
+    override fun boolVal() = elements.isNotEmpty()
+    override fun toString() = value
+}
