326f141f972d8dcded70280362b19c6dbae5af95
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-05-21 23:58:00 +0800

    Fixed: Printing them showed their token instead of name

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/ClassObj.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/ClassObj.kt
@@ -5,7 +5,7 @@ import com.vinkami.vinkamilang.language.parse.node.ClassNode
 
 class ClassObj(val node: ClassNode): BaseObject {
     override val type = "Class"
-    override val value = node.name
+    override val value = node.name.value
     override val startPos = node.startPos
     override val endPos = node.endPos
     override val property = Referables(null, isRoot=false)
--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/FuncObj.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/FuncObj.kt
@@ -5,7 +5,7 @@ import com.vinkami.vinkamilang.language.parse.node.FuncNode
 
 class FuncObj(val node: FuncNode): BaseObject {
     override val type = "Function"
-    override val value = node.name
+    override val value = node.name.value
     override val startPos = node.startPos
     override val endPos = node.endPos
     override val property = Referables(null, isRoot=false)
