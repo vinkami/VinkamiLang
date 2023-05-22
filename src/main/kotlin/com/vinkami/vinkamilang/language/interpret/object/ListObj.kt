0be5f1736c139199f894c461ec2491a58f710830
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-05-22 17:45:25 +0800

    Changed: Lists are not Arrays

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/ListObj.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/ListObj.kt
@@ -4,7 +4,7 @@ import com.vinkami.vinkamilang.language.interpret.Referables
 import com.vinkami.vinkamilang.language.lex.Position
 
 class ListObj(val elements: List<BaseObject>, override val startPos: Position, override val endPos: Position): BaseObject {
-    override val type = "Array"
+    override val type = "List"
     override val value = elements.joinToString(", ", "[", "]")
     override val property = Referables(null, isRoot=false)
     override fun boolVal() = elements.isNotEmpty()
