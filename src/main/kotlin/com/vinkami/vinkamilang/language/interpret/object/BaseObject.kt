5a02ddb9fa1f1c3c80a14da09e2d658f6511fc64
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-05-22 18:18:25 +0800

    Added: and, or Added: +[] == 0 Changed: boolVal() -> boolVal

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/BaseObject.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/BaseObject.kt
@@ -10,7 +10,8 @@ interface BaseObject {
     val startPos: Position
     val endPos: Position
     val property: Referables
-    fun boolVal(): Boolean = true
+    val boolVal: Boolean
+        get() = true
 
     fun plus(other: BaseObject): BaseObject = throw SyntaxError("Can't add $this and $other", startPos, endPos)
     fun minus(other: BaseObject): BaseObject = throw SyntaxError("Can't subtract $this by $other", startPos, endPos)
@@ -19,8 +20,8 @@ interface BaseObject {
     fun mod(other: BaseObject): BaseObject = throw SyntaxError("Can't get remainder of $this divided by $other", startPos, endPos)
     fun power(other: BaseObject): BaseObject = throw SyntaxError("Can't get $this to the power of $other", startPos, endPos)
 
-    fun unaryPlus(): BaseObject = throw SyntaxError("Can't apply unary plus to $this", startPos, endPos)
-    fun unaryMinus(): BaseObject = throw SyntaxError("Can't apply unary minus to $this", startPos, endPos)
+    fun unaryPlus(): BaseObject = if (boolVal) NumberObj(1f, startPos, endPos) else NumberObj(0f, startPos, endPos)  // hi JS
+    fun unaryMinus(): BaseObject = if (boolVal) NumberObj(-1f, startPos, endPos) else NumberObj(0f, startPos, endPos)
 
     fun equal(other: BaseObject): BaseObject = BoolObj(value == other.value, startPos, other.endPos)
     fun notEqual(other: BaseObject): BaseObject = BoolObj(value != other.value, startPos, other.endPos)
@@ -28,4 +29,7 @@ interface BaseObject {
     fun greaterEqual(other: BaseObject): BaseObject = throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
     fun less(other: BaseObject): BaseObject = throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
     fun greater(other: BaseObject): BaseObject = throw SyntaxError("Can't compare $this and $other", startPos, other.endPos)
+
+    fun and(other: BaseObject): BaseObject = BoolObj(boolVal && other.boolVal, startPos, other.endPos)
+    fun or(other: BaseObject): BaseObject = BoolObj(boolVal || other.boolVal, startPos, other.endPos)
 }
