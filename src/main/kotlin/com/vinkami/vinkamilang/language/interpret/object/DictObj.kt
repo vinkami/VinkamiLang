3f4cdffe0cbe52fe788311c7b5ec963a92fd2438
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-05-22 17:44:23 +0800

    Changed: dictionaries can now get printed like actual code

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/DictObj.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/DictObj.kt
@@ -5,7 +5,7 @@ import com.vinkami.vinkamilang.language.lex.Position
 
 class DictObj(val elements: Map<BaseObject, BaseObject>, override val startPos: Position, override val endPos: Position): BaseObject {
     override val type = "Dict"
-    override val value = elements.toString()
+    override val value = elements.entries.joinToString(", ", "[", "]") { "${it.key}: ${it.value}" }
     override val property = Referables(null, isRoot=false)
     override fun boolVal() = elements.isNotEmpty()
     override fun toString() = value
