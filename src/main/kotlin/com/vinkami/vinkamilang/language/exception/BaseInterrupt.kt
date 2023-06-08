7fe49d12a64cb979f49651fa297049c6069ea657
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-06-09 03:41:44 +0800

    Removed: waiting for development

--- src/main/kotlin/com/vinkami/vinkamilang/language/exception/BaseInterrupt.kt
+++
@@ -1,5 +0,0 @@
-package com.vinkami.vinkamilang.language.exception
-
-import com.vinkami.vinkamilang.language.lex.Position
-
-class BaseInterrupt(message: String, startPos: Position, endPos: Position): BaseLangException(message, startPos, endPos)
