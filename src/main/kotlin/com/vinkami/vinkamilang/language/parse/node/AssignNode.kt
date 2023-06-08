a29edee6e41e2ea4b76e1e18595327c63ab3392e
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-06-09 03:42:25 +0800

    Changed: mutability check

--- src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/AssignNode.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/parse/node/AssignNode.kt
@@ -1,10 +1,9 @@
 package com.vinkami.vinkamilang.language.parse.node
 
 import com.vinkami.vinkamilang.language.lex.Position
-import com.vinkami.vinkamilang.language.lex.Token
 
-class AssignNode(val iden: IdenNode, val assignToken: Token, val value: BaseNode, startPos: Position): BaseNode(startPos, value.endPos) {
+class AssignNode(val iden: IdenNode, val value: BaseNode, val mutable: Boolean, startPos: Position): BaseNode(startPos, value.endPos) {
     override fun toString(): String {
-        return "($iden $assignToken $value)$callStr"
+        return "(${if (mutable) "var" else "val"} $iden = $value)$callStr"
     }
 }
