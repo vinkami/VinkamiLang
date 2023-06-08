8e753f10a8ce27105a3d99cb4871d40755744ee2
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-06-09 03:41:28 +0800

    Changed: putting exceptions into BaseObject to have everything single-rooted

--- src/main/kotlin/com/vinkami/vinkamilang/language/exception/BaseLangException.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/exception/BaseLangException.kt
@@ -1,14 +1,21 @@
 package com.vinkami.vinkamilang.language.exception
 
+import com.vinkami.vinkamilang.language.interpret.Referables
+import com.vinkami.vinkamilang.language.interpret.`object`.BaseObject
 import com.vinkami.vinkamilang.language.lex.Position
 
-abstract class BaseLangException(message: String, private val startPos: Position, private val endPos: Position): Exception(message) {
+abstract class BaseLangException(message: String, override val startPos: Position, override val endPos: Position): BaseObject, Exception(message) {
+    override val type = "Error"
+    override val value = message
+    override val property = Referables(null, isRoot=false)
+    override val boolVal = true
+
     override fun toString(): String {
         return """
             ${this::class.simpleName}: $message
-            File: ${startPos.fileName}, line: ${startPos.line + 1}
-            
-            ${startPos.code.split('\n')[startPos.line]}
+            Traceback:
+                File: ${startPos.fileName}, line: ${startPos.line + 1}
+                    ${startPos.code.split('\n')[startPos.line].trimIndent()}
             ${makeArrow(startPos, endPos)}
         """.trimIndent()
     }
