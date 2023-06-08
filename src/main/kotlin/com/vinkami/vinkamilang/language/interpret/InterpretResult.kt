33394fe9c178eca1c2c037b8dc7235d3b45b2aa6
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-06-09 03:36:23 +0800

    WIP: Actually making the interruption system

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/InterpretResult.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/InterpretResult.kt
@@ -1,20 +1,18 @@
 package com.vinkami.vinkamilang.language.interpret
 
 import com.vinkami.vinkamilang.language.exception.BaseError
-import com.vinkami.vinkamilang.language.exception.BaseInterrupt
 import com.vinkami.vinkamilang.language.interpret.`object`.BaseObject
+import com.vinkami.vinkamilang.language.lex.TokenType
 
 class InterpretResult() {
     lateinit var error: BaseError
     lateinit var obj: BaseObject
-    var interrupt: BaseInterrupt? = null
+    var interrupt: TokenType? = null  // BREAK / RETURN
 
     val hasError: Boolean get() = ::error.isInitialized
     val hasObject: Boolean get() = ::obj.isInitialized
 
-    constructor(obj: BaseObject): this() {this(obj)}
     constructor(error: BaseError?): this() {this(error)}
-    constructor(interrupt: BaseInterrupt): this() {this(interrupt)}
 
     operator fun invoke(obj: BaseObject) = apply {  // on success
         this.obj = obj
@@ -22,18 +20,5 @@ class InterpretResult() {
 
     operator fun invoke(error: BaseError?) = apply {// on failure
         if (error != null) this.error = error
-    }
-
-    operator fun invoke(res: InterpretResult): InterpretResult {  // push error through nested parsing; won't make sense in a constructor
-        if (res.hasError) this.error = res.error
-        return res
-    }
-
-    operator fun invoke(interrupt: BaseInterrupt) = apply {  // on interrupt
-        this.interrupt = interrupt
-    }
-
-    fun clearInterrupt() = apply {
-        interrupt = null
     }
 }
