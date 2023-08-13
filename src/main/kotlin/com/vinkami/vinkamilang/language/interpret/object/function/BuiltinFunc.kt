0c51de17fd0b0ed40028dcf855ecdffd8eea7cc6
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-08-14 02:29:14 +0800

    Added: comments for startPos, endPos Added: actual startPos, endPos when invoked

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/function/BuiltinFunc.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/object/function/BuiltinFunc.kt
@@ -3,18 +3,19 @@ package com.vinkami.vinkamilang.language.interpret.`object`.function
 import com.vinkami.vinkamilang.language.Constant.builtinPos
 import com.vinkami.vinkamilang.language.interpret.Referables
 import com.vinkami.vinkamilang.language.interpret.`object`.BaseObject
+import com.vinkami.vinkamilang.language.lex.Position
 
 abstract class BuiltinFunc(val name: String): BaseObject {
     override val type = "Function"
     override val value = name
-    override val startPos = builtinPos
-    override val endPos = builtinPos
+    override val startPos = builtinPos  // these startPos and endPos are to act as a placeholder for default values of parameters.
+    override val endPos = builtinPos    // the actual startPos and endPos are passed in the invoke() function.
     override val property = Referables(null, isRoot=false)
 
     override fun toString(): String {
         return "<function $name>"
     }
 
-    abstract operator fun invoke(ref: Referables): BaseObject
+    abstract operator fun invoke(ref: Referables, startPos: Position, endPos: Position): BaseObject
     abstract val parameters: List<Parameter>
 }
