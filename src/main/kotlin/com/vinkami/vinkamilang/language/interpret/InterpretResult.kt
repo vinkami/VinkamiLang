24104f688798082fdf4feade85c0ad7fa0868ae3
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-08-14 02:29:33 +0800

    Added: interruption check

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/InterpretResult.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/InterpretResult.kt
@@ -1,11 +1,16 @@
 package com.vinkami.vinkamilang.language.interpret
 
+import com.vinkami.vinkamilang.language.exception.NotYourFaultError
 import com.vinkami.vinkamilang.language.interpret.`object`.BaseObject
 import com.vinkami.vinkamilang.language.interpret.`object`.NullObj
 import com.vinkami.vinkamilang.language.lex.TokenType
 
 class InterpretResult(val obj: BaseObject, var interrupt: TokenType? = null) {
     // interrupt: TT.BREAK / TT.RETURN
+
+    init {
+        if (interrupt !in listOf(null, TokenType.BREAK, TokenType.RETURN)) throw NotYourFaultError("Invalid interrupt type: $interrupt", obj.startPos, obj.endPos)
+    }
 
     val hasInterrupt: Boolean
         get() = interrupt != null
