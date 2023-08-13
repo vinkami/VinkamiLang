a6b250c90adfc04ec00c4a90e8f82262ade52ad6
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-08-13 23:26:59 +0800

    Removed: legacy code about errors in InterpretResult and those lateinit stuff

--- src/main/kotlin/com/vinkami/vinkamilang/language/interpret/InterpretResult.kt
+++ src/main/kotlin/com/vinkami/vinkamilang/language/interpret/InterpretResult.kt
@@ -1,35 +1,19 @@
 package com.vinkami.vinkamilang.language.interpret
 
-import com.vinkami.vinkamilang.language.exception.BaseError
 import com.vinkami.vinkamilang.language.interpret.`object`.BaseObject
+import com.vinkami.vinkamilang.language.interpret.`object`.NullObj
 import com.vinkami.vinkamilang.language.lex.TokenType
 
-class InterpretResult() {
-    lateinit var error: BaseError
-    private lateinit var _obj: BaseObject
+class InterpretResult(val obj: BaseObject, var interrupt: TokenType? = null) {
+    // interrupt: TT.BREAK / TT.RETURN
 
-    val obj: BaseObject
-        get() {
-            if (::_obj.isInitialized) return _obj
-            if (hasError) throw error
-            throw IllegalAccessError("Accessing boject of an interpret() but got no object nor error. Find vinkami for help.")
-        }
-    var interrupt: TokenType? = null  // BREAK / RETURN
+    val hasInterrupt: Boolean
+        get() = interrupt != null
 
-    val hasError: Boolean get() = ::error.isInitialized
-    val hasObject: Boolean get() {
-        if (::_obj.isInitialized) return true
-        if (hasError) throw error
-        return false
-    }
+    val hasObject: Boolean
+        get() = obj !is NullObj
 
-    constructor(obj: BaseObject) : this() { this(obj) }
-
-    operator fun invoke(obj: BaseObject) = apply {  // on success
-        if (obj is BaseError) {
-            this.error = obj
-        } else {
-            this._obj = obj
-        }
+    fun clearInterrupt() = apply {
+        interrupt = null
     }
 }
